24e60dc0221914804e4a43dcf4c8ccbfbbafc9dc
justinfranzen <39416475+justinfranzen@users.noreply.github.com>
2018-06-28 13:04:40 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,33 @@
+<div id="footer">
+
+    <div id="footer-location-info">
+    
+        <p>Hartford<br>
+        17 N Main St, Hartford, WI 53027<br>
+        <a href="262-483-5955" class="footer-link">(262)-483-5955</a></p>
+        
+        <p>Fredonia<br>
+        211 Highland Dr, Fredonia, WI 53021<br>
+        <a href="262-483-5955" class="footer-link">(262)-483-5955</a></p>
+        
+        <p>West Bend - Kettle Moraine YMCA<br>
+        1111 W Washington St, West Bend, WI 53095<br>
+        <a href="414-721-6177" class="footer-link">(414)-721-1677</a></p>
+        
+        <p>Brookfield<br>
+        19700 W Bluemound Rd, Brookfield, WI 53045<br>
+        <a href="262-483-1265" class="footer-link">(262)-483-1265</a></p>
+    
+    </div>
+    
+        <ul id="programs">
+            <h3>ATKD Programs</h3>
+            <li><a href="taekwondo.php">Taekwondo</a></li>
+            <li><a href="testing.php">Testing</a></li>
+            <li><a href="competition-team.php">Competition Team</a></li>
+            <li><a href="birthday-parties.php">Birthday Parties</a></li>
+            <li><a href="class-schedule.php">Class Schedule</a></li>
+        </ul>
+
+
+</div>
